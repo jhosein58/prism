fbacdfc09a6309af877c36ca1c7ea4b0a08648c8
jhosein58 <jhosein58@gmail.com>
2026-07-05 01:20:34 +0330

    amade sazi winit

---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+use std::sync::Arc;
+
+use winit::{
+    application::ApplicationHandler,
+    event::*,
+    event_loop::ActiveEventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window,
+};
+
+// hame state ha ro inja negah midarim
+pub struct Prism {
+    window: Arc<Window>,
+    // TODO: bayad eezafe beshe state-e WGPU
+    // ...
+}
+
+impl Prism {
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        Ok(Self { window })
+    }
+
+    pub fn resize(&mut self, _width: u32, _height: u32) {}
+
+    pub fn render(&mut self) {
+        self.window.request_redraw();
+    }
+}
+
+pub struct App {
+    state: Option<Prism>,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self { state: None }
+    }
+}
+
+impl ApplicationHandler<Prism> for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        #[allow(unused_mut)]
+        let mut window_attributes = Window::default_attributes();
+
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+
+        self.state = Some(pollster::block_on(Prism::new(window)).unwrap());
+    }
+
+    #[allow(unused_mut)]
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: Prism) {
+        self.state = Some(event);
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: WindowEvent,
+    ) {
+        let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => state.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => {
+                state.render();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => match (code, key_state.is_pressed()) {
+                (KeyCode::Escape, true) => event_loop.exit(),
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use prism::App;
+use winit::event_loop::EventLoop;
+
 fn main() {
-    println!("Hello, prism!");
+    env_logger::init();
+    let event_loop = EventLoop::with_user_event().build().unwrap();
+    let mut app = App::new();
+
+    // chon hich surface-i nadarim va adaptor gpu ro amade nakardim chizi neshoon nemidee
+    event_loop.run_app(&mut app).unwrap();
 }
